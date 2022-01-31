d4aa2c56c9fd1e813f07afb5200080501925fb7f
Marco Kwan <marcokwan10@gmail.com>
2022-01-31 01:19:57 -0500

    updated createBill with state

--- components/CreateBill.js
+++ components/CreateBill.js
@@ -1,15 +1,17 @@
 import { Box, Button, Center, FormControl, Heading, Input, Text, Pressable } from "native-base";
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateBill() {
+	const [billName, setBillName] = useState("");
+
 	return (
 		<Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
 			<Center>
 				<FormControl mb="48" pr="5">
 					<Text fontSize="4xl">Bill Name</Text>
-					<Input variant="underlined" width="64" fontSize="24" />
+					<Input variant="underlined" width="64" fontSize="24" value={billName} onChangeText={(v) => setBillName(v)} />
 					<FormControl.HelperText mb="8">Give your bill a name.</FormControl.HelperText>
-					<Pressable>
+					<Pressable onPress={() => console.log("👋 billName ------>", billName)}>
 						<Text fontSize="2xl">Next</Text>
 					</Pressable>
 				</FormControl>
