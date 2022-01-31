a8cc1e2c49fe1851c8276551b6b39f4b8095c3f1
Marco Kwan <marcokwan10@gmail.com>
2022-01-30 23:53:31 -0500

    home screen and stack navigation

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "expo-status-bar";
+
+import Home from "./components/Home";
+import Color from "./components/Color";
+
+//stack navigation
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NativeBaseProvider>
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="Home" component={Home} options={{ header: () => null }} />
+					<Stack.Screen name="Color" component={Color} />
+				</Stack.Navigator>
+			</NavigationContainer>
+			<StatusBar style="auto" />
+		</NativeBaseProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Home.js
@@ -0,0 +1,16 @@
+import { Box, Button, Heading } from "native-base";
+import { Alert } from "react-native";
+
+export default function Home({ navigation }) {
+	return (
+		<Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+			<Heading>Bill Splitter</Heading>
+			<Button variant="link" size="lg" onPress={() => navigation.navigate("Color")}>
+				Color
+			</Button>
+			<Button variant="link" size="lg" onPress={() => Alert.alert("adding bill")}>
+				Add Bill
+			</Button>
+		</Box>
+	);
+}
