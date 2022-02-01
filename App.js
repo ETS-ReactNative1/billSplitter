dbf9d466df4ab802408a4436bc2e1cec0432f4f4
Johan Perez <1johanperez@gmail.com>
2022-01-31 21:28:05 -0500

    working on OCR

--- App.js
+++ App.js
@@ -1,28 +1,37 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NativeBaseProvider } from "native-base";
-import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NativeBaseProvider } from 'native-base';
+import { StatusBar } from 'expo-status-bar';
 
-import Home from "./components/Home";
-import Color from "./components/Color";
-import CreateBill from "./components/CreateBill";
-import Camera from "./components/Camera";
+import Home from './components/Home';
+import Color from './components/Color';
+import CreateBill from './components/CreateBill';
+import Camera from './components/Camera';
+import OCRComponent from './components/OCRComponent';
 
 //stack navigation
 const Stack = createStackNavigator();
 
 export default function App() {
-	return (
-		<NativeBaseProvider>
-			<NavigationContainer>
-				<Stack.Navigator>
-					<Stack.Screen name="Home" component={Home} options={{ header: () => null }} />
-					<Stack.Screen name="Color" component={Color} />
-					<Stack.Screen name="CreateBill" component={CreateBill} />
-					<Stack.Screen name="Camera" component={Camera} />
-				</Stack.Navigator>
-			</NavigationContainer>
-			<StatusBar style="auto" />
-		</NativeBaseProvider>
-	);
+  return (
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ header: () => null }}
+          />
+          <Stack.Screen name="Color" component={Color} />
+          <Stack.Screen name="CreateBill" component={CreateBill} />
+          <Stack.Screen name="Camera" component={Camera} />
+          <Stack.Screen
+            name="OCRComponent"
+            component={OCRComponent}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </NativeBaseProvider>
+  );
 }
--- components/Camera.js
+++ components/Camera.js
@@ -1,65 +1,93 @@
-import React, { useState, useEffect } from "react";
-import { Camera } from "expo-camera";
-import { View, Container, Text, Pressable, Image } from "native-base";
+import React, { useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
+import { View, Container, Text, Pressable, Image } from 'native-base';
 
 export default function App({ navigation }) {
-	const [hasPermission, setHasPermission] = useState(null);
-	const [type, setType] = useState(Camera.Constants.Type.back);
-	const [camera, setCamera] = useState(null);
-	const [image, setImage] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
 
-	useEffect(() => {
-		(async () => {
-			const { status } = await Camera.requestCameraPermissionsAsync();
-			setHasPermission(status === "granted");
-		})();
-	}, []);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
 
-	const capture = async () => {
-		if (camera) {
-			const data = await camera.takePictureAsync(null);
-			setImage(data.uri);
-			//The image uri is in the image state after capture
-			//You can pass the image to a new view by using navigation
-			console.log("👋  ------>", image); //--> prints the captured image uri
-		}
-	};
+  const capture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      console.log('This is data in the Camera Component', data);
+      setImage(data.uri);
+      console.log(
+        'This is Image in the camera Component, after setting Image',
+        image,
+      );
+      //The image uri is in the image state after capture
+      //You can pass the image to a new view by using navigation
 
-	if (hasPermission === null) {
-		return <View />;
-	}
-	if (hasPermission === false) {
-		return <Text>No access to camera</Text>;
-	}
+      //1 pass image to OCR Component via navigation
+      //2 do something and verify we are getting that image
 
-	//this shows the image after capture delete if not needed
-	if (image) {
-		return <Image source={{ uri: image }} flex={1} alt="receipt" />;
-	}
+      navigation.navigate('OCRComponent', image);
+    }
+  };
 
-	return (
-		<View flex={1}>
-			<Camera style={{ flex: 1 }} type={type} ref={(ref) => setCamera(ref)}>
-				<Container flex={1} backgroundColor="transparent" flexDirection={"row"} mb="10" ml="5">
-					<Pressable
-						flex={0.1}
-						alignSelf="flex-end"
-						alignItems="center"
-						onPress={() => {
-							setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
-						}}
-					>
-						<Text color="white" fontSize="lg">
-							Flip
-						</Text>
-					</Pressable>
-					<Pressable flex={0.9} alignSelf="flex-end" alignItems="center" onPress={() => capture()}>
-						<Text color="white" fontSize="lg">
-							Capture
-						</Text>
-					</Pressable>
-				</Container>
-			</Camera>
-		</View>
-	);
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  //this shows the image after capture delete if not needed
+  if (image) {
+    return <Image source={{ uri: image }} flex={1} alt="receipt" />;
+  }
+
+  return (
+    <View flex={1}>
+      <Camera
+        style={{ flex: 1 }}
+        type={type}
+        ref={(ref) => setCamera(ref)}
+      >
+        <Container
+          flex={1}
+          backgroundColor="transparent"
+          flexDirection={'row'}
+          mb="10"
+          ml="5"
+        >
+          <Pressable
+            flex={0.1}
+            alignSelf="flex-end"
+            alignItems="center"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back,
+              );
+            }}
+          >
+            <Text color="white" fontSize="lg">
+              Flip
+            </Text>
+          </Pressable>
+          <Pressable
+            flex={0.9}
+            alignSelf="flex-end"
+            alignItems="center"
+            onPress={() => capture()}
+          >
+            <Text color="white" fontSize="lg">
+              Capture
+            </Text>
+          </Pressable>
+        </Container>
+      </Camera>
+    </View>
+  );
 }
