13b8cb9566c0ccebd3ed9770e20a75f8f7450f79
Marco Kwan <marcokwan10@gmail.com>
2022-02-08 13:51:58 -0500

    added ayer and header styling

--- App.js
+++ App.js
@@ -1,39 +1,38 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NativeBaseProvider } from 'native-base';
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "expo-status-bar";
 
-import Home from './components/Home';
-import Color from './components/Color';
-import CreateBill from './components/CreateBill';
-import Camera from './components/Camera';
-import BillScreen from './components/BillPage/BillScreen';
-import FinalizedBill from './components/FinalizedBill';
+import Home from "./components/Home";
+import Color from "./components/Color";
+import CreateBill from "./components/CreateBill";
+import Camera from "./components/Camera";
+import BillScreen from "./components/BillPage/BillScreen";
+import FinalizedBill from "./components/FinalizedBill";
 
 //stack navigation
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ header: () => null }}
-          />
-          <Stack.Screen name="Color" component={Color} />
-          <Stack.Screen name="CreateBill" component={CreateBill} />
-          <Stack.Screen name="Camera" component={Camera} />
-          <Stack.Screen name="BillScreen" component={BillScreen} />
-          <Stack.Screen
-            name="FinalizedBill"
-            component={FinalizedBill}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </NativeBaseProvider>
-  );
+	return (
+		<NativeBaseProvider>
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName="Home"
+					screenOptions={{
+						headerTintColor: "white",
+						headerStyle: { backgroundColor: "#06b6d4" },
+					}}
+				>
+					<Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+					<Stack.Screen name="Color" component={Color} />
+					<Stack.Screen name="CreateBill" component={CreateBill} />
+					<Stack.Screen name="Camera" component={Camera} options={{ title: "Capture" }} />
+					<Stack.Screen name="BillScreen" component={BillScreen} />
+					<Stack.Screen name="FinalizedBill" component={FinalizedBill} />
+				</Stack.Navigator>
+			</NavigationContainer>
+			<StatusBar style="auto" />
+		</NativeBaseProvider>
+	);
 }
