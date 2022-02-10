c8d9564cc76b95ccc6fc753ebad201d71a1aaf0a
Marco Kwan <marcokwan10@gmail.com>
2022-02-09 21:25:57 -0500

    added validation to create bill component

--- components/CreateBill.js
+++ components/CreateBill.js
@@ -1,9 +1,32 @@
-import { Box, Button, Center, FormControl, Heading, Input, Text, Pressable, KeyboardAvoidingView } from "native-base";
+import { Button, Center, FormControl, Input, Text, KeyboardAvoidingView, Container } from "native-base";
 import React, { useState } from "react";
+import { Alert } from "react-native";
 
 export default function CreateBill({ navigation }) {
 	const [billName, setBillName] = useState("");
+	const [billNameErr, setBillNameErr] = useState(false);
 	const [payer, setPayer] = useState("");
+	const [payerErr, setPayerErr] = useState(false);
+
+	const nextAlert = () => {
+		Alert.alert("Field Required", "Please specify bill Name and payer.");
+	};
+
+	const errorMessage = () => {
+		if (!billName) setBillNameErr(true);
+		if (!payer) setPayerErr(true);
+	};
+
+	const pressHandle = () => {
+		if (!billName.length || !payer.length) {
+			setBillNameErr(false);
+			setPayerErr(false);
+			errorMessage();
+			nextAlert();
+			return;
+		}
+		navigation.navigate("Camera", { billName, payer });
+	};
 
 	return (
 		<KeyboardAvoidingView behavior="padding" flex={1} bg="#fff" alignItems="center" justifyContent="center">
@@ -18,7 +41,10 @@ export default function CreateBill({ navigation }) {
 						value={billName}
 						onChangeText={(v) => setBillName(v)}
 					/>
-					<FormControl.HelperText mb="8">Give your bill a name</FormControl.HelperText>
+					<Container mb={8}>
+						<FormControl.HelperText>Give your bill a name</FormControl.HelperText>
+						{billNameErr && <Text color={"red.500"}>Required*</Text>}
+					</Container>
 					<Text fontSize="4xl">Payer</Text>
 					<Input
 						fontWeight={"light"}
@@ -28,8 +54,11 @@ export default function CreateBill({ navigation }) {
 						value={payer}
 						onChangeText={(v) => setPayer(v)}
 					/>
-					<FormControl.HelperText mb={12}>Specify the payer of the bill</FormControl.HelperText>
-					<Button onPress={() => navigation.navigate("Camera", { billName, payer })}>
+					<Container mb={8}>
+						<FormControl.HelperText>Specify the payer of the bill</FormControl.HelperText>
+						{payerErr && <Text color={"red.500"}>Required*</Text>}
+					</Container>
+					<Button onPress={pressHandle}>
 						<Text fontSize="xl" color="white">
 							Next
 						</Text>
